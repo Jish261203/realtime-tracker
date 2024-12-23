851bf181a3fe0ca0bddf79571910744485f55f29
jishnay nair <jish261203@gmail.com>
2024-12-23 17:48:42 +0530

    Maded Real-Time Tracker

--- public/js/script.js
+++ public/js/script.js
@@ -10,7 +10,7 @@ if (navigator.geolocation) {
 	navigator.geolocation.watchPosition(
 		(position) => {
 			const { latitude, longitude } = position.coords;
-			socket.emit("send-locations", { latitude, longitude });
+			socket.emit("send-location", { latitude, longitude });
 		},
 		(error) => {
 			console.error(error);
